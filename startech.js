a8a982b1d3c6a6682e3a78149f58db49f042adfb
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2021-04-11 20:52:24 +0600

    more coming later

--- startech.js
+++ startech.js
@@ -1,11 +1,16 @@
-const pup = require('puppeteer');
-(async () => {
-    try{
-        const browser = await puppeteer.launch({headless: false})
-        const page = await browser.newPage()
+require('dotenv').config()
+const puppeteer = require('puppeteer');
+const searchKeyword = 'keyboard';
 
+(async () => {
+    try {
+      const browser = await puppeteer.launch({headless: false});
+      const page = await browser.newPage();
+      await page.goto('https://www.startech.com.bd/', {waitUntil: 'load', timeout:0})
+      await page.type('#search > div.input-group > input[type=text]', searchKeyword)
+      await page.click('#search > div.input-group > button')
+    } catch (error) {
+      console.log(error);
     }
-    catch (error) {
-        console.log(error);
-    }
-})
+    
+})();
