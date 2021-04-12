28d267d6eb7d212cd61318a4abbb99d8a7fa896b
nohanmohsin <67507872+nohanmohsin@users.noreply.github.com>
2021-04-12 21:56:19 +0600

    done ig

--- startech.js
+++ startech.js
@@ -6,9 +6,18 @@ const searchKeyword = 'keyboard';
     try {
       const browser = await puppeteer.launch({headless: false});
       const page = await browser.newPage();
+      await page.setViewport({
+        width: 1280,
+        height: 720
+      })
       await page.goto('https://www.startech.com.bd/', {waitUntil: 'load', timeout:0})
       await page.type('#search > div.input-group > input[type=text]', searchKeyword)
       await page.click('#search > div.input-group > button')
+      await page.waitForSelector('#content > div.search-products.margin-top.product-listing.m-t-15 > div.row > div:nth-child(1) > div > div.product-info > div.actions > div.price > span:nth-child(1)')
+      
+      let priceEl= await page.$x('//*[@id="content"]/div[2]/div[2]/div[1]/div/div[2]/div[2]/div[1]/span[1]')
+      let realPrice=await page.evaluate(el => el.textContent, priceEl[0])
+      console.log(realPrice)
     } catch (error) {
       console.log(error);
     }
